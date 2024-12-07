2a59f5b9902a7ad6289157886574ff781b1ade9e
Nguyen Huu Loc <locn562836@gmail.com>
2024-12-07 10:06:30 +0700

    Update: Alter donation table columns to change data types to float4

---
+++ supabase/migrations/20241207030211_alter_donation_change_datatypes.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.donation ALTER COLUMN mask TYPE float4 USING mask::float4;
+ALTER TABLE public.donation ALTER COLUMN rice TYPE float4 USING rice::float4;
+ALTER TABLE public.donation ALTER COLUMN noodle TYPE float4 USING noodle::float4;
+ALTER TABLE public.donation ALTER COLUMN vegetable TYPE float4 USING vegetable::float4;
+ALTER TABLE public.donation ALTER COLUMN meat TYPE float4 USING meat::float4;
