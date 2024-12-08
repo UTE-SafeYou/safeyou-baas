3b9924d30e1033d6ff3dfa37e6704fa0bf911c4f
Nguyen Huu Loc <locn562836@gmail.com>
2024-12-08 11:58:27 +0700

    Add SQL migrations to delete document and vector_documents tables, and to add last_location, latitude, and longitude to user_profiles

---
+++ supabase/migrations/20241208043943_delete_document_vector.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS public.documents;
+DROP TABLE IF EXISTS public.vector_documents;
---
+++ supabase/migrations/20241208045342_add_user_last_location.sql
@@ -0,0 +1,3 @@
+ALTER TABLE public.user_profiles ADD last_location postgis.geography NULL;
+ALTER TABLE public.user_profiles ADD latitude float4 NULL;
+ALTER TABLE public.user_profiles ADD longitude float4 NULL;
