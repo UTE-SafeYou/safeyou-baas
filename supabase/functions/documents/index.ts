7bfc31d23254d715b68418f1e118a14c85f03dfa
Nguyen Huu Loc <locn562836@gmail.com>
2024-12-09 16:21:40 +0700

    Add copyright notices and licensing information to various service files

--- supabase/functions/documents/index.ts
+++ supabase/functions/documents/index.ts
@@ -1,7 +1,21 @@
+// SafeYou
+// Copyright (C) 2024  SafeYou
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import { createClient } from 'jsr:@supabase/supabase-js@2'
 import express from 'npm:express@4.18.2'
-import { createClient, SupabaseClient } from 'jsr:@supabase/supabase-js@2'
-import opencage from 'npm:opencage-api-client@1.0.7'
-import { rankResults, cleanText } from './utils/text-processing.ts'
+import { cleanText } from './utils/text-processing.ts'
 
 const app = express()
 app.use(express.json())
--- supabase/functions/place/index.ts
+++ supabase/functions/place/index.ts
@@ -1,8 +1,19 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+// SafeYou
+// Copyright (C) 2024  SafeYou
 
-// Setup type definitions for built-in Supabase Runtime APIs
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { SupabaseService } from '../services/supabase.service.ts';
 const corsHeaders = {
